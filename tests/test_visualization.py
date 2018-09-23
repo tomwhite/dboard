ca3e99fc8ef13e3b8eddf92027b20b437a739e7c
Tom White <tom.e.white@gmail.com>
2018-09-23 21:05:01 +0100

    Fix test

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,6 +1,6 @@
-from dboard.visualization import dboard
+from dboard.visualization import create_json_index
 
 
 def test_dboard(tmpdir):
-    dboard("tests/entries.csv", str(tmpdir), bg_range=(3.9, 8))
-    assert "TIR: 78.8%" in tmpdir.join("index.html").read()
+    create_json_index("tests/entries.csv", str(tmpdir), bg_range=(3.9, 8))
+    assert '"tir": "78.8%"' in tmpdir.join("index.json").read()
