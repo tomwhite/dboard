583a16b05f104dbd859ef71987d745e2a30f78db
Tom White <tom.e.white@gmail.com>
2018-09-23 17:59:47 +0100

    Upload to gcloud

--- dboard/files.py
+++ dboard/files.py
@@ -2,5 +2,10 @@ import os
 
 
 def open_file(filename, mode="r"):
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    return open(filename, mode)
+    if filename.startswith("gs://"):
+        import gcsfs
+        fs = gcsfs.GCSFileSystem(token='google_default')
+        return fs.open(filename, mode)
+    else:
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        return open(filename, mode)
--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 from dboard.visualization import create_json_index
 
 csv_file = '/tmp/entries.csv'
-out_dir = 'out'
+out_dir = 'gs://tom-dboard'
 bg_range = (3.9, 8)
 
 create_json_index(csv_file, out_dir, bg_range)
